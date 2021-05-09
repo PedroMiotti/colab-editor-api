879008744389831155c59b253e08d8eac8392d4f
Pedro <pedromiotti7@gmail.com>
2021-05-09 18:54:58 -0300

    Leaving the current doc room before joining another

--- src/services/file.service.js
+++ src/services/file.service.js
@@ -4,9 +4,9 @@ const { RoomModel } = require("../models/index");
 
 exports.createFile = async (fileName, namespaceId, io) => {
   try {
-    if (!fileName || !namespaceId) 
+    if (!fileName || !namespaceId)
       console.log("Nome do arquivo e a sala sao obrigatorios !");
-    
+
     let room = await RoomModel.findOne({ namespaceId: namespaceId });
 
     if (!room) console.log("Sala nao existe");
@@ -24,43 +24,57 @@ exports.createFile = async (fileName, namespaceId, io) => {
   }
 };
 
-exports.joinFile = async (prevFile, currentFile, namespaceId, socketId, socket, io) => {
+exports.joinFile = async (
+  prevFile,
+  currentFile,
+  namespaceId,
+  socketId,
+  socket,
+  io
+) => {
   try {
-    if (!currentFile || !namespaceId) 
+    if (!currentFile || !namespaceId)
       console.log("Nome do arquivo e a sala sao obrigatorios !");
-    
+
     let room = await RoomModel.findOne({ namespaceId: namespaceId });
 
     if (!room) console.log("Sala nao existe");
-    
-    let socket_rooms = io.of(namespaceId).adapter.rooms
+
+    let socket_rooms = io.of(namespaceId).adapter.rooms;
     // Check if user is already inside a room that is not the default.
     for (let [key, value] of socket_rooms) {
-        if (key !== socketId && value.has(socketId)) { 
-            socket.leave(prevFile);
-        }
-    }  
+      if (key !== socketId && value.has(socketId)) {
+        socket.leave(prevFile);
+      }
+    }
 
-    io.of(namespaceId).to(currentFile).emit(SocketEvents.SERVER_USER_JOINED_FILE, {});
-    
+    io.of(namespaceId).to(currentFile).emit(SocketEvents.SERVER_USER_JOINED_FILE, 'message', (fileContent) => {
+      console.log(fileContent)
+    });
+
     socket.join(currentFile);
-
   } catch (e) {
-      console.log(e);
+    console.log(e);
   }
 };
 
-exports.updateCode = async (fileName, code, socketId, namespaceId, socket, io) => {
-    try {
-      if (!fileName || !namespaceId) 
-        console.log("Nome do arquivo e a sala sao obrigatorios !");
-      
-      io.of(namespaceId).to(fileName).emit(SocketEvents.SERVER_UPDATE_CODE, {
-        value: code,
-        socketId: socketId
-      });
-      
-    } catch (e) {
-        console.log(e);
-    }
-  };
+exports.updateCode = async (
+  fileName,
+  code,
+  socketId,
+  namespaceId,
+  socket,
+  io
+) => {
+  try {
+    if (!fileName || !namespaceId)
+      console.log("Nome do arquivo e a sala sao obrigatorios !");
+
+    io.of(namespaceId).to(fileName).emit(SocketEvents.SERVER_UPDATE_CODE, {
+      value: code,
+      socketId: socketId,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
