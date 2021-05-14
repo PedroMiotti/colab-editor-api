a030e5ec213a6a0f0732f098697e1ade96e5b598
Pedro <pedromiotti7@gmail.com>
2021-05-14 09:54:27 -0300

    Add docker

--- src/infra/mongo.js
+++ src/infra/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 require('dotenv').config();
 
-mongoose.connect(`mongodb://${process.env.MONGO_URI}/${process.env.MONGO_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(`mongodb://${process.env.MONGO_URI}/${process.env.MONGO_NAME}`, { useNewUrlParser: true });
 
 mongoose.Promise = global.Promise;
 
